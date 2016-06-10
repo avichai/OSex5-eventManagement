6bac8a7d4d80cd4d121a30d0d8b48c533ee833b3
roeeCates <roee.cates@mail.huji.ac.il>
2016-06-10 17:29:06 +0300

    getNextToken - a new function in Utils (used to parse easily).
    I think the synchronization between the read and write is done by the OS (this the all point of sockets)

--- Client.cpp
+++ Client.cpp
@@ -12,14 +12,25 @@
 #define SERVER_ADDR_INDEX 2
 #define SERVER_PORT_INDEX 3
 
+#define REGISTER_CMD "REGISTER"
+#define CREATE_CMD "CREATE"
+#define GET_TOP_5_CMD "GET_TOP_5"
+#define SEND_RSVP_CMD "SEND_RSVP"
+#define GET_RSVPS_LIST_CMD "GET_RSVPS_LIST"
+#define UNREGISTER_CMD "UNREGISTER"
+
+#define MAX_RESPONSE_LEN 99999
+
 
 using namespace std;
 
-// can give sa as arg?
+
+
+
 int callSocket(struct sockaddr_in sa) {
     int s;
 
-    s = socket(AF_INET, SOCK_STREAM, 0);
+    s = socket(AF_INET, SOCK_STREAM, 0);// todo
 
     int n = connect(s, (struct sockaddr*)&sa, sizeof(sa)); //todo
 
@@ -30,8 +41,77 @@ int callSocket(struct sockaddr_in sa) {
     return s;
 }
 
+/*
+ * Runs the client.
+ */
+void clientRun(string clientName, struct sockaddr_in serverAddr) {
+    bool isRegistered = false;
 
 
+    string input, cmd, delim = SPACE, message, args;
+    const char* cmdC;
+    size_t pos;
+    int serverS;
+    bool stillRunning = true, isCmd;
+    while (stillRunning) {
+        isCmd = true;
+        args = "";
+
+        getline(cin, input);
+        cmdC = getNextToken().c_str();
+        if (strcasecmp(cmdC, REGISTER_CMD) == 0) {
+            if (isRegistered) {
+
+            }
+            isRegistered = true;
+        }
+        else if (strcasecmp(cmdC, CREATE_CMD) == 0) {
+            if (!isRegistered) {
+
+            }
+        }
+        else if (strcasecmp(cmdC, GET_TOP_5_CMD) == 0) {
+            if (!isRegistered) {
+
+            }
+
+        }
+        else if (strcasecmp(cmdC, SEND_RSVP_CMD) == 0) {
+            if (!isRegistered) {
+
+            }
+
+        }
+        else if (strcasecmp(cmdC, GET_RSVPS_LIST_CMD) == 0) {
+            if (!isRegistered) {
+
+            }
+
+        }
+        else if (strcasecmp(cmdC, UNREGISTER_CMD) == 0) {
+
+        }
+        else {
+            isCmd = false;
+        };
+
+
+        if (isCmd) {
+            message = cmd + " " + clientName + " " + args;
+            serverS = callSocket(serverAddr);
+            writeData(serverS, message);
+            char response[MAX_RESPONSE_LEN];
+            readData(serverS, response, MAX_RESPONSE_LEN);
+        }
+
+        stillRunning = false;
+    }
+}
+
+
+/*
+ * Client main function.
+ */
 int main(int argc, char *argv[]) {
     unsigned short portNum;
     if ((argc != CLIENT_VALID_NARGS) ||
@@ -41,16 +121,16 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    struct sockaddr_in sa;
-    sa.sin_addr.s_addr = inet_addr(argv[SERVER_ADDR_INDEX]);
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons(portNum);
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_addr.s_addr = inet_addr(argv[SERVER_ADDR_INDEX]);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(portNum);
 
-    int s = callSocket(sa);//todo
+    string clientName = string(argv[CLIENT_NAME_INDEX]);
 
-    writeData(s, "blabla");
+    // runs the client
+    clientRun(clientName, serverAddr);
 
-    close(s); //todo
-    cout << "client finished!!!" << endl;
+    cout << "client - " << clientName << " exit!!" << endl;
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@
 using namespace std;
 
 int main() {
-    int n =1;
-    cout << (1 >NULL) << endl;
+
+    string input, command;
+    size_t pos = 0;
+    bool stillRunning = true;
+    while (stillRunning) {
+        getline(cin, input);
+        pos = input.find(" ");
+        command = input.substr(0,pos);
+        cout << "!!! " << command << " !!!" << endl;
+
+        stillRunning = false;
+    }
+
+
     return 0;
 }
